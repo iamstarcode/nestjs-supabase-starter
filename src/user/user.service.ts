7ad5fed554d9dd49ce2e9989942c4460629cc1a3
StarCode <iamstarcode@gmail.com>
2024-05-13 13:28:40 +0100

    chore: update user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { SupabaseService } from 'src/supabase/supabase.service';
-import { Tables } from 'src/supabase/types/db.types';
 
 @Injectable()
 export class UserService {
   constructor(private readonly supabaseService: SupabaseService) {}
 
-  async getUserProfile() {
-    const { data } = await this.supabaseService
-      .getClient()
-      .from('profiles')
-      .select('*')
-      .single();
+  async getUser() {
+    const {
+      data: { user },
+    } = await this.supabaseService.getClient().auth.getUser();
 
-    const profile: Tables<'profiles'> = data;
-
-    return { profile };
+    return { user };
   }
 }
