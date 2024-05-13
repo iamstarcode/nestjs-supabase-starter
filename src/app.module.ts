12b2db4c6b17f7cb65ef0121ec1e674d49d7f467
StarCode <iamstarcode@gmail.com>
2024-05-13 16:43:49 +0100

    chore: update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SupabaseModule } from './supabase/supabase.module';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
-import { UserService } from './user/user.service';
 
 @Module({
   imports: [
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { SupabaseGuard } from 'src/supabase/guards/supabase.guard';
 import { UserService } from './user.service';
-import { SupabaseService } from 'src/supabase/supabase.service';
 
 @Controller('user')
 @UseGuards(SupabaseGuard)
